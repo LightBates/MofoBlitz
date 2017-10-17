d7e01bf4782d275348236167818c78090d1c76df
Light <lcsbates@gmail.com>
2017-10-17 08:31:06 -0600

    Range Detection Script didn't add, oops

---
+++ Assets/_Scripts/RangeDetection.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeDetection : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+			gameObject.GetComponentInParent<EnemyBehavior>().InRange = true;
+			gameObject.GetComponentInParent<EnemyBehavior>().player = collider.gameObject;
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+			gameObject.GetComponentInParent<EnemyBehavior>().InRange = false;
+		}
+	}
+}
